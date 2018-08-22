4a25d5f7781329a009014a3986e389a434762c7e
JasonFuuu <31625628+JasonFuuu@users.noreply.github.com>
2018-08-22 09:34:09 +0800

    Update Main.java

--- src/com/tao/Main.java
+++ src/com/tao/Main.java
@@ -3,6 +3,6 @@ package com.tao;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Git Git2...");
+        System.out.println("Git Git3...");
     }
 }
